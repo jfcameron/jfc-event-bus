bbfd8bea44581f033edec79acfe0b17f419ff53c
jfcameron <jf.cameron@gmail.com>
2020-10-04 18:38:00 -0400

    initial commit

---
+++ test/event_bus_test.cpp
@@ -0,0 +1,31 @@
+// © 2020 Joseph Cameron - All Rights Reserved
+
+#include <string>
+
+#include <jfc/catch.hpp>
+#include <jfc/types.h>
+
+
+using namespace gdk;
+
+TEST_CASE("event_bus", "[jfc::event_bus]")
+{
+    //auto bus = eventbus...
+    //REQUIRE(pContext);
+
+    SECTION("copy semantics work")
+    {
+        /*auto pCamera = pContext->make_camera();
+
+        REQUIRE(pCamera);
+        REQUIRE(!jfc::glGetError());
+
+        auto pCamera2 = pContext->make_camera(*pCamera);
+
+        REQUIRE(pCamera2);
+        REQUIRE(!jfc::glGetError());*/
+    }
+
+    //SECTION("move semantics work") ....
+}
+
