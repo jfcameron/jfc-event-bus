b54d4afcd71996d9f061c960e9e0f115e5327a6e
jfcameron <jf.cameron@gmail.com>
2020-10-05 07:14:52 -0400

    whitespace + comment cleanup

--- include/jfc/event_bus.h
+++ include/jfc/event_bus.h
@@ -10,61 +10,60 @@
 
 namespace jfc 
 {
-	/// \brief mechanism for producers to push events to a certalized bus, 
-    /// which are then seen by observers
-	template<class event_type_template>
-	class event_bus final
-	{
-	public:
-		using event_type = event_type_template;
+    /// \brief mechanism to message events to any number of observers
+    template<class event_type_template>
+    class event_bus final
+    {
+        public:
+            using event_type = event_type_template;
 
-		using observer_type = std::function<void(event_type)>;
+            using observer_type = std::function<void(event_type)>;
 
-		using observer_weak_ptr_type = std::weak_ptr<observer_type>;
+            using observer_weak_ptr_type = std::weak_ptr<observer_type>;
 
-		using observer_shared_ptr_type = std::shared_ptr<observer_type>;
+            using observer_shared_ptr_type = std::shared_ptr<observer_type>;
 
-	private:
-		mutable std::vector<observer_weak_ptr_type> m_Observers;
+        private:
+            mutable std::vector<observer_weak_ptr_type> m_Observers;
 
-	public:
-		/// \brief notify all observers of the event
-		/// \remark removes observers that are null
-		void propagate_event(event_type e) const
-		{
-			for (size_t i(0); i < m_Observers.size(); ++i)
-			{
-				if (auto p = m_Observers[i].lock())
-				{
-					(*p)(e);
-				}
-				else
-				{
-					m_Observers.erase(m_Observers.begin() + i);
-				}
-			}
-		}
+        public:
+            /// \brief notify all observers of the event
+            /// \remark removes observers that are null
+            void propagate_event(event_type e) const
+            {
+                for (size_t i(0); i < m_Observers.size(); ++i)
+                {
+                    if (auto p = m_Observers[i].lock())
+                    {
+                        (*p)(e);
+                    }
+                    else
+                    {
+                        m_Observers.erase(m_Observers.begin() + i);
+                    }
+                }
+            }
 
-		/// \brief adds an observer, to be notified when an event occurs
-		void add_observer(observer_weak_ptr_type observer)
-		{
-			m_Observers.push_back(observer);
-		}
+            /// \brief adds an observer, to be notified when an event occurs
+            void add_observer(observer_weak_ptr_type observer)
+            {
+                m_Observers.push_back(observer);
+            }
 
-        /// \brief explicit move semantics
-        event_bus<event_type> &operator=(event_bus<event_type> &&) = default;
-        /// \brief explicit move semantics
-        event_bus(event_bus<event_type> &&) = default;
-        
-        /// \brief explicit copy semantics
-        event_bus<event_type> &operator=(const event_bus<event_type> &) = default;
-        /// \brief explicit copy semantics
-        event_bus(const event_bus<event_type> &) = default;
+            /// \brief explicit move semantics
+            event_bus<event_type> &operator=(event_bus<event_type> &&) = default;
+            /// \brief explicit move semantics
+            event_bus(event_bus<event_type> &&) = default;
 
-        event_bus() = default;
+            /// \brief explicit copy semantics
+            event_bus<event_type> &operator=(const event_bus<event_type> &) = default;
+            /// \brief explicit copy semantics
+            event_bus(const event_bus<event_type> &) = default;
 
-        ~event_bus() = default;
-	};
+            event_bus() = default;
+
+            ~event_bus() = default;
+    };
 }
 
 #endif
